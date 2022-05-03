7f523f7f6d5c5a8adf11ea4bc69b4daf38cfcda9
Sergey Salomatin <salomatinsv8@gmail.com>
2022-05-03 12:43:09 +0300

    created core date with saving added tasks

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -6,22 +6,27 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
 
-    var toDoItems: [String] = []
+    var toDoItems: [Task] = []
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        getAllItems()
     }
+    
     @IBAction func addTask(_ sender: Any) {
         let alert = UIAlertController(title: "Add Task", message: "add new task", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
         let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
-            let textField = alert.textFields?.first
-            self.toDoItems.insert((textField?.text)!, at: 0)
-            self.tableView.reloadData()
+            let textField = alert.textFields?.first?.text // first not working??
+            if let newTask = textField {
+                self.saveTask(taskToDo: newTask)
+                self.tableView.reloadData()
+            }
         }
         
         alert.addTextField { _ in
@@ -31,6 +36,35 @@ class TableViewController: UITableViewController {
         alert.addAction(okAction)
         alert.addAction(cancelAction)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveTask(taskToDo: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)
+        let taskObject = NSManagedObject(entity: entity!, insertInto: context) as! Task
+        taskObject.taskToDo = taskToDo
+        
+        do {
+            try context.save()
+            toDoItems.append(taskObject)
+            print("saved")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func getAllItems() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            try context.save()
+            toDoItems = try context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
     // MARK: - Table view data source
@@ -47,7 +81,8 @@ class TableViewController: UITableViewController {
 //        var content = cell.defaultContentConfiguration()
 //        content.text = toDoitems[indexPath.row]
         
-        cell.textLabel?.text = toDoItems[indexPath.row]
+        let task = toDoItems[indexPath.row]
+        cell.textLabel?.text = task.taskToDo
         
         return cell
     }
